322895b5a1306c72e08e3d1f8d7d933ec5423598
MSR <96829843+MSAuthentic@users.noreply.github.com>
2022-04-22 09:41:17 +0200

    First Commit

---
+++ src/WebPolls/WebPolls/Server/Controllers/ProfileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPolls.Server.Controllers
+{
+  public class ProfileController : Controller
+  {
+    // GET: ProfileController
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+ 
+
+    // POST: ProfileController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    // GET: ProfileController/Delete/5
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+ 
+  }
+}
